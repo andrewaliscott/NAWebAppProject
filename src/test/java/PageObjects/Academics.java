4bc5fdb8433040b0d547003d644173165e83bf1d
sapexk <52297254+sapexk@users.noreply.github.com>
2019-11-18 07:25:18 -0600

    Update Academics.java

--- src/test/java/PageObjects/Academics.java
+++ src/test/java/PageObjects/Academics.java
@@ -3,11 +3,6 @@ package PageObjects;
 import org.openqa.selenium.By;
 
 public class Academics {
-    public By getAcademics() {
-        return academics;
-    }
-
-    private By academics=By.linkText ( "Academics" );
 
     }
 
